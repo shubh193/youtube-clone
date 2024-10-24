5ffb03c8096d59b6972808733ed016d14e0fec8f
shubh193 <shubhcse193@gmail.com>
2024-10-24 09:01:16 +0530

    nested comments

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -38,12 +38,13 @@ const CommentsData = [
   {
     name: "Shubham Sahni",
     text: "Lorem ipsum dolor sit amet.",
-    replies: ["shubham"],
+    replies: [], // Make sure all replies are arrays, not strings.
   },
 ];
 
+// Single Comment Component
 const Comment = ({ data }) => {
-  const { name, text, replies } = data;
+  const { name, text } = data;
   return (
     <div className="flex my-4 ml-5 shadow-sm bg-gray-100 p-2 rounded-lg">
       <img
@@ -58,13 +59,32 @@ const Comment = ({ data }) => {
     </div>
   );
 };
-const CommentsList = () => {};
 
+// Component to render list of comments and their replies
+const CommentsList = ({ comments }) => {
+  return (
+    <>
+      {comments.map((comment, index) => (
+        <div key={index}>
+          <Comment data={comment} />
+          {/* Recursively render replies only if they exist and are an array */}
+          {comment.replies && comment.replies.length > 0 && (
+            <div className="ml-5 border border-l-black pl-5">
+              <CommentsList comments={comment.replies} />
+            </div>
+          )}
+        </div>
+      ))}
+    </>
+  );
+};
+
+// Main Comments Container
 const CommentsContainer = () => {
   return (
     <div className="m-5 p-2">
       <h1 className="font-bold text-2xl">Comments</h1>
-      <Comment data={CommentsData[0]} />
+      <CommentsList comments={CommentsData} />
     </div>
   );
 };
